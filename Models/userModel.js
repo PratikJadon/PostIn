005448bf707291d6c1146f75fa89ca8fc689b344
Pratik Jadon <pratikucskmpj@gmail.com>
2024-01-31 16:51:35 +0530

    .env update, added DB Atlas connection

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-const UserModel = new mongoose.Schema([
+const UserSchema = new mongoose.Schema([
     {
         name:{
             type:String,
@@ -22,4 +22,4 @@ const UserModel = new mongoose.Schema([
         }
     }
 ])
-export  default mongoose.model("User",UserModel);
+export default mongoose.model("User",UserSchema);
