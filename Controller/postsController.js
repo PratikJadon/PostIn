eeac4d6f6de2adc383dd163fda044b9d55c5ca16
Rivansh <rivansh63@gmail.com>
2024-02-29 10:09:58 +0530

    Added Skeleton for posts

---
+++ Controller/postsController.js
@@ -0,0 +1,18 @@
+export default class postController{
+    static async addPost(req,res){
+        
+    }
+    static async getAllPost(req,res){
+
+    }
+    static async deletePost(req,res){
+
+    }
+    static async updatePost(req,res){
+
+    }
+    static async getPostByID(req,res){
+        
+    }
+
+}
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express  from "express";
 //Routes
 import userRouter from "./Routes/userRoutes.js";
+import postRouter from "./Routes/postsRoutes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/users",userRouter);
+app.use("/api/posts",postRouter);
+
+
 
 export default app;
